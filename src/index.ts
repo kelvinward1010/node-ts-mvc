d5b9c37c636d6071203792af7b6bff8e97f430b5
Kelvin Ward <kelvinward1010@gmail.com>
2024-07-08 19:14:16 +0700

    update config package

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,20 @@ import { connectDatabase } from "./config/database";
 import { routes } from "./routes";
 // import swaggerDocument from './config/swagger';
 
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Middleware xử lý dữ liệu form-urlencoded
-dotenv.config();
 connectDatabase();
 // swaggerDocument(app, Number(port));
 routes(app);
 
 app.get("/", (req, res) => {
-    res.send("Hello, TypeScript with Express!");
+    res.send("Hello, TypeScript with Express on Nodejs!");
 });
 
 app.listen(port, () => {
