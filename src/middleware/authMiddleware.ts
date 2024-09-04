48fe2af220f65abefbaa0d3f489dde888e807a90
Kelvin Ward <kelvinward1010@gmail.com>
2024-09-04 16:40:53 +0700

    update auth middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,32 +1,38 @@
 import { Request, Response, NextFunction } from "express";
+import { userModel } from "../models/UserModel";
 import jwt from "jsonwebtoken";
-
 
 export const authenticateToken = async (
     req: Request,
     res: Response,
-    next: NextFunction
+    next: NextFunction,
 ) => {
     try {
         const secretKey: any = process.env.JWT_ACCESSTOKEN;
-        const token = req.headers.authorization?.split(' ')[1];
+        const token = req.headers.authorization?.split(" ")[1];
 
         if (!token) {
-            return res.status(401).json({ message: 'Unauthorized' });
+            return res.status(401).json({ message: "Unauthorized" });
         }
 
-        jwt.verify(token, secretKey, (err: any, decoded: any) => {
+        jwt.verify(token, secretKey, async (err: any, userID: any) => {
             if (err) {
-                if (err.name === 'TokenExpiredError') {
-                    return res.status(401).json({ message: 'Token has expired' });
+                if (err.name === "TokenExpiredError") {
+                    return res
+                        .status(401)
+                        .json({ message: "Token has expired" });
                 }
-                return res.status(403).json({ message: 'Invalid token' });
+                return res.status(403).json({ message: "Invalid token" });
             }
 
-            req.body = {...req.body, decoded};
+            const user = await userModel.findOne({
+                _id: userID,
+            });
+
+            req.body = { ...req.body, userID, user };
             next();
         });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: "Internal server error" });
     }
 };
