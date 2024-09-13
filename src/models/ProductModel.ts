6757fb4948a66ea82afe8d476ea28d2a542b5f90
Kelvin Ward <kelvinward1010@gmail.com>
2024-09-13 17:11:26 +0700

    update module product

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
-        image: { type: String, required: true },
+        image: { type: String },
         description: { type: String, required: true },
         quantity: { type: Number, required: true },
         price: { type: Number, required: true },
